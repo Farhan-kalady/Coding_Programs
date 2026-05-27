bee5284790f43b53005a618543fd52055b9b3423
Farhan-kalady <farhancherushola@gmail.com>
2026-05-27 17:43:33 +0530

    Recursion parameter

--- Recursion.py
+++ Recursion.py
@@ -15,3 +15,16 @@ def recursion(x,n):
     recursion(x, n-1)
     
 recursion("farhan",4)    
+
+def recu(sum, i, n):
+    if i > n:
+        print(sum)
+        return
+    recu(sum + i, i + 1, n)
+recu(0,1,10)    
+
+def sum(n):
+    if n == 1:
+        return 1
+    return n + sum(n - 1)
+print(sum(10))
